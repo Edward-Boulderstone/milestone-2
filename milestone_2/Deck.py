0a7ac5ee09f60365764c5d0fde1fcb6a48d45ce3
Edward-Boulderstone <Edward.Boulderstone@bjss.com>
2022-10-20 16:19:06 +0100

    Reformated class names to be captialised

---
+++ milestone_2/Deck.py
@@ -0,0 +1,20 @@
+suits = ("Spade", "Club", "Hearts", "Diamonds")
+face_values = (
+    "Ace",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "Jack",
+    "Queen",
+    "King",
+)
+
+
+class deck:
+    pass
--- milestone_2/dealer.py
+++
@@ -1,2 +0,0 @@
-class dealer:
-    pass

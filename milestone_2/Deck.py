5e7afa95ec21b01b8f45af96008a28cd97343029
Edward Boulderstone <Edward.Boulderstone@Bjss.com>
2022-10-20 17:01:35 +0100

    Added failing tests for Card class
    Signed-off-by: Edward Boulderstone <Edward.Boulderstone@Bjss.com>

--- milestone_2/Deck.py
+++ milestone_2/Deck.py
@@ -1,20 +1,2 @@
-suits = ("Spade", "Club", "Hearts", "Diamonds")
-face_values = (
-    "Ace",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "Jack",
-    "Queen",
-    "King",
-)
-
-
 class Deck:
     pass

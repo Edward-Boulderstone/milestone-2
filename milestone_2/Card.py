00b3613acf0e417e448e509f7246e92392b00a85
Edward Boulderstone <Edward.Boulderstone@Bjss.com>
2022-10-21 09:57:12 +0100

    Added mypy to pre-commit
    Signed-off-by: Edward Boulderstone <Edward.Boulderstone@Bjss.com>

--- milestone_2/Card.py
+++ milestone_2/Card.py
@@ -1,22 +1,12 @@
-suits = ("Spade", "Club", "Heart", "Diamond")
-face_values = (
-    "Ace",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "Jack",
-    "Queen",
-    "King",
-)
+from Card_Values import suits
+from Card_Values import face_values
 
 
 class Card:
+    """
+    A Card object represents a playing card from a 52 card tarot deck
+    """
+
     def __init__(self, suit: str, value: int):
         assert suit in suits
         assert value in range(len(face_values))

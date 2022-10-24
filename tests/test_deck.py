b5e837fe0441d584743deecc36f67df45e64d5ce
Edward Boulderstone <Edward.Boulderstone@Bjss.com>
2022-10-24 17:14:25 +0100

    Added docstrings to deck test methods
    Signed-off-by: Edward Boulderstone <Edward.Boulderstone@Bjss.com>

--- tests/test_deck.py
+++ tests/test_deck.py
@@ -9,11 +9,17 @@ def deck_size() -> int:
 
 
 def test_deck_initializes_with_52_unique_cards() -> None:
+    """
+    Tests that the deck initializes with 52 cards unique cards
+    """
     assert len(Deck()) == deck_size()
     assert len(set(Deck())) == deck_size()
 
 
 def test_deck_removes_card_when_drawn() -> None:
+    """
+    Tests that the deck removes cards when they are drawn from it
+    """
     deck = Deck()
     current_deck_size = deck_size()
     cards_in_deck = []
@@ -25,4 +31,7 @@ def test_deck_removes_card_when_drawn() -> None:
 
 
 def test_deck_throws_assertion_error_when_no_cards() -> None:
+    """
+    Tests that the deck throws an error when you try to draw from it when it has no cards
+    """
     assert False

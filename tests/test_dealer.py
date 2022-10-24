07ecbd9f2044bcf119a7c87fe0bc52d3fddcb03e
Edward Boulderstone <Edward.Boulderstone@Bjss.com>
2022-10-24 17:11:30 +0100

    Added docstrings for dealer test cases
    Signed-off-by: Edward Boulderstone <Edward.Boulderstone@Bjss.com>

--- tests/test_dealer.py
+++ tests/test_dealer.py
@@ -2,6 +2,9 @@ from milestone_2 import Dealer
 
 
 def test_average_card_delt():
+    """
+    Tests that the dealer will deal all the cards in the deck
+    """
     dealer = Dealer()
     cards_to_deal = 5200
     cards_delt = [dealer.deal_card() for _ in range(cards_to_deal)]
@@ -10,6 +13,9 @@ def test_average_card_delt():
 
 
 def test_starting_hand_deal():
+    """
+    Tests that the dealer will deal a starting hand of two cards to each player
+    """
     dealer = Dealer()
     starting_hand = dealer.deal_starting_hand()
     assert len(starting_hand) == 2

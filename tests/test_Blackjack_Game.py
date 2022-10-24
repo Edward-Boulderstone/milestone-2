0a0efe1eac8fea39c78595ab8639f8aba529c371
Edward Boulderstone <Edward.Boulderstone@Bjss.com>
2022-10-24 17:07:26 +0100

    Added doc strings to test_Blackjack_game
    Signed-off-by: Edward Boulderstone <Edward.Boulderstone@Bjss.com>

--- tests/test_Blackjack_Game.py
+++ tests/test_Blackjack_Game.py
@@ -11,16 +11,28 @@
 
 
 def test_stand_and_hit() -> None:
+    """
+    End-to-end test to validate that the user can stand and hit from any hand
+    """
     assert False
 
 
 def test_can_bet_various() -> None:
+    """
+    End-to-end test to validate that the user can bet any quantity of funds
+    """
     assert False
 
 
 def test_tracks_players_money() -> None:
+    """
+    End-to-end test to validate that the user has the total quantity of their funds tracked
+    """
     assert False
 
 
 def test_feedback_on_win_or_loss() -> None:
+    """
+    End-to-end test to validate that the user gets a message when they win or lose
+    """
     assert False

0f2eb8dd87d6d069aaf943656584b364683defcf
Edward Boulderstone <Edward.Boulderstone@Bjss.com>
2022-10-25 12:56:19 +0100

    Wrote code that passed the blackjack absract class tests
    Signed-off-by: Edward Boulderstone <Edward.Boulderstone@Bjss.com>

--- milestone_2/Blackjack_Player.py
+++ milestone_2/Blackjack_Player.py
@@ -1,17 +1,31 @@
+from milestone_2.Dealer import Dealer
+
+
 class Blackjack_Player:
     actions = ["hit", "stand"]
 
+    def __init__(self):
+        self.hand = Dealer().deal_starting_hand()
+        self.is_stood = False
+
     def is_bust(self) -> bool:
-        raise NotImplementedError
+        return self.hand.is_bust()
 
     def hit(self) -> None:
-        raise NotImplementedError
+        if not self.can_perform_actions():
+            return
+        self.hand.draw(Dealer().deal_card())
 
     def stand(self) -> None:
-        raise NotImplementedError
+        if not self.can_perform_actions():
+            return
+        self.is_stood = True
 
     def handle_turn(self) -> None:
         raise NotImplementedError
 
     def reset_for_next_game(self) -> None:
-        raise NotImplementedError
+        self.hand = Dealer().deal_starting_hand()
+
+    def can_perform_actions(self) -> bool:
+        return not (self.hand.is_bust() or self.is_stood)

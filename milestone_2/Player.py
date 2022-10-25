f2e0c08684f637502f06f19c37787e2c04f2730f
Edward Boulderstone <Edward.Boulderstone@Bjss.com>
2022-10-25 12:56:19 +0100

    Wrote code that passed the blackjack absract class tests
    Signed-off-by: Edward Boulderstone <Edward.Boulderstone@Bjss.com>

--- milestone_2/Player.py
+++ milestone_2/Player.py
@@ -1,27 +1,11 @@
 from milestone_2.Blackjack_Player import Blackjack_Player
-from milestone_2.Dealer import Dealer
-from milestone_2.Hand import Hand
 from milestone_2.Wallet import Wallet
 
 
 class Player(Blackjack_Player):
     def __init__(self):
+        super().__init__()
         self.wallet = Wallet()
-        self.hand = Dealer().deal_starting_hand()
-        self.is_stood = False
-
-    def is_bust(self) -> bool:
-        return self.hand.is_bust()
-
-    def hit(self) -> None:
-        if not self.can_perform_actions():
-            return
-        self.hand.draw(Dealer().deal_card())
-
-    def stand(self) -> None:
-        if not self.can_perform_actions():
-            return
-        self.is_stood = True
 
     def handle_turn(self) -> None:
         if not self.can_perform_actions:
@@ -29,12 +13,6 @@ class Player(Blackjack_Player):
         if self.user_input().lower() == "hit":
             return self.hit()
         return self.stand()
-
-    def reset_for_next_game(self) -> None:
-        self.hand = Dealer().deal_starting_hand()
-
-    def can_perform_actions(self) -> bool:
-        return not (self.hand.is_bust() or self.is_stood)
 
     def user_input(self) -> str:
         user_input = ""
--- tests/test_player.py
+++ tests/test_player.py
@@ -30,51 +30,6 @@ def mock_user_input(mocked_input: str) -> None:
     monkeypatch_session.setattr("builtins.input", mock_input)
 
 
-def test_is_bust(player, bust_player: Player) -> None:
-    """
-    Tests that a player can be identified as bust
-    Args:
-        player: A player with a non-bust hand
-        bust_player: A player with a bust hand
-    """
-    assert not player.is_bust()
-    assert bust_player.is_bust()
-
-
-def test_can_hit(player) -> None:
-    """
-    Tests that a player can add a card to their hand
-    Args:
-        player: Player with non-bust hand
-    """
-    initial_cards_in_hand = len(player.hand.hand)
-    player.hit()
-    assert initial_cards_in_hand + 1 == len(player.hand.hand)
-
-
-def test_can_stand(player) -> None:
-    """
-    Tests that a player can stand, and this prevents them from hitting
-    Args:
-        player:  Player with a non-bust hand
-    """
-    initial_cards_in_hand = len(player.hand.hand)
-    player.stand()
-    player.hit()
-    assert initial_cards_in_hand == len(player.hand.hand)
-
-
-def test_cant_make_actions_when_bust(bust_player: Player) -> None:
-    """
-    Tests that a bust player cannot hit
-    Args:
-        bust_player:  Player with a bust hand
-    """
-    initial_cards_in_hand = len(bust_player.hand.hand)
-    bust_player.hit()
-    assert initial_cards_in_hand == len(bust_player.hand.hand)
-
-
 def test_user_can_choose_to_hit_on_their_turn(
     player,
 ) -> None:
@@ -101,24 +56,3 @@ def test_user_can_choose_to_legally_stand_on_their_turn(
     player.stand = MagicMock()
     player.handle_turn()
     player.stand.assert_called_with()
-
-
-def test_player_resets_hand(player):
-    """
-    Tests that when the player has their hand reset, they get a new hand with 2 cards in it
-    Args:
-        player_with_starting_hand: A generic player
-    """
-    first_hand = player.hand
-    player.reset_for_next_game()
-    assert not first_hand == player.hand
-    assert len(player.hand.hand) == 2
-
-
-def test_player_initial_hand(player):
-    """
-    Tests that when the player gets their initial hand it has two cards in it
-    Args:
-        player_with_starting_hand: A freshly initialised player
-    """
-    assert len(player.hand.hand) == 2

2534df2894530abb9193ec2b903bd654a9da5bd9
Edward Boulderstone <Edward.Boulderstone@Bjss.com>
2022-10-25 13:41:20 +0100

    Moved end to end tests to seperate file
    Signed-off-by: Edward Boulderstone <Edward.Boulderstone@Bjss.com>

---
+++ tests/test_end_to_end.py
@@ -0,0 +1,38 @@
+"""
+
+    You need to create a simple text-based BlackJack game
+    The game needs to have one player versus an automated dealer.
+    The player can stand or hit.
+    The player must be able to pick their betting amount.
+    You need to keep track of the player's total money.
+    You need to alert the player of wins, losses, or busts, etc...
+
+"""
+
+
+def test_stand_and_hit() -> None:
+    """
+    End-to-end test to validate that the user can stand and hit from any hand
+    """
+    assert False
+
+
+def test_can_bet_various() -> None:
+    """
+    End-to-end test to validate that the user can bet any quantity of funds
+    """
+    assert False
+
+
+def test_tracks_players_money() -> None:
+    """
+    End-to-end test to validate that the user has the total quantity of their funds tracked
+    """
+    assert False
+
+
+def test_feedback_on_win_or_loss() -> None:
+    """
+    End-to-end test to validate that the user gets a message when they win or lose
+    """
+    assert False

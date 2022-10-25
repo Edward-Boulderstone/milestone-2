13a79e41ff21710d6aaa2c54bb33efd44a95fd23
Edward Boulderstone <Edward.Boulderstone@Bjss.com>
2022-10-25 11:26:00 +0100

    Added code to pass the dealer test cases
    Signed-off-by: Edward Boulderstone <Edward.Boulderstone@Bjss.com>

--- milestone_2/Dealer.py
+++ milestone_2/Dealer.py
@@ -1,2 +1,25 @@
+from milestone_2.Card import Card
+from milestone_2.Deck import Deck
+
+
 class Dealer:
-    pass
+    def __init__(self):
+        self.deck = Deck()
+
+    def deal_card(self) -> Card:
+        """
+        Draws and deals a card from a deck, if there are no cards in a deck, a new one is created
+        Returns: Random card from a deck
+
+        """
+        try:
+            return self.deck.draw()
+        except AssertionError:
+            self.deck = Deck()
+        return self.deck.draw()
+
+    def deal_starting_hand(self) -> tuple[Card, Card]:
+        """
+        Returns: A starting hand for a game of blackjack
+        """
+        return self.deal_card(), self.deal_card()
